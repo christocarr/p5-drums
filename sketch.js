5abbc9ca5ff5ced76f38697241ee45765e0b74d6
christocarr <christocarr@outlook.com>
2020-05-21 11:49:23 +0100

    Move sound loop creation into mouseclicked function

--- sketch.js
+++ sketch.js
@@ -1,35 +1,44 @@
 
-// let hh;
-// let hPat;
-// let hPhrase;
-// let drums
-
-// function setup() {
-
-//   createCanvas(400, 400)
-//   hh = loadSound('assets/hh_sample.mp3', () => {
-//     drums.loop()
-//   })
-
-//   hPat = [1, 1, 1, 1];
-
-//   hPhrase = new p5.Phrase('hh', () => {
-//     hh.play()
-//   }, hPat)
-
-//   drums = new p5.Part()
-//   drums.addPhrase(hPhrase)
-
-// }
-
-// function draw() {
-//   background(220)
-// }
+let hh;
+let hPat;
+let hPhrase;
+let drums;
 
 function setup() {
+
   createCanvas(400, 400)
+  // hh = loadSound('assets/hh_sample.mp3', () => {
+  //   drums.loop()
+  // })
+
+  // hPat = [1, 1, 1, 1];
+
+  // hPhrase = new p5.Phrase('hh', () => {
+  //   hh.play()
+  // }, hPat)
+
+  // drums = new p5.Part()
+  
+  // drums.addPhrase(hPhrase)
 }
 
 function draw() {
   background(220)
+}
+
+function mouseClicked() {
+  hh = loadSound('assets/hh_sample.mp3', () => {
+    drums.loop()
+  })
+
+  hPat = [1, 0, 1, 0];
+
+  hPhrase = new p5.Phrase('hh', (time) => {
+    hh.play(time)
+    
+  }, hPat)
+
+  drums = new p5.Part()
+  
+  drums.addPhrase(hPhrase)
 }
