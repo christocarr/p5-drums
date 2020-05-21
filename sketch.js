0dc7616e693c983b0c1e963b586cd42cb6a11571
christocarr <christocarr@outlook.com>
2020-05-21 08:27:10 +0100

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,35 @@
+
+// let hh;
+// let hPat;
+// let hPhrase;
+// let drums
+
+// function setup() {
+
+//   createCanvas(400, 400)
+//   hh = loadSound('assets/hh_sample.mp3', () => {
+//     drums.loop()
+//   })
+
+//   hPat = [1, 1, 1, 1];
+
+//   hPhrase = new p5.Phrase('hh', () => {
+//     hh.play()
+//   }, hPat)
+
+//   drums = new p5.Part()
+//   drums.addPhrase(hPhrase)
+
+// }
+
+// function draw() {
+//   background(220)
+// }
+
+function setup() {
+  createCanvas(400, 400)
+}
+
+function draw() {
+  background(220)
+}
