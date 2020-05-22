0e8202707b1d07a903ca9c2fc79121b3f4aee4c1
christocarr <christocarr@outlook.com>
2020-05-22 09:52:34 +0100

    Add keypressed function to play and stop loop

--- sketch.js
+++ sketch.js
@@ -4,21 +4,46 @@ let hPat, cPat, bPat; //pattern for beats that can be changed
 let hPhrase, cPhrase, bPhrase;
 let drums;
 
+function preload() {
+  hh = loadSound('assets/hh_sample.mp3')
+
+  clap = loadSound('assets/clap_sample.mp3')
+
+  bass = loadSound('assets/bass_sample.mp3')
+}
+
 function setup() {
-  hh = loadSound('assets/hh_sample.mp3', () => {
-    
-  })
 
-  clap = loadSound('assets/clap_sample.mp3', () => {
-    
-  })
+  // hPat = [1, 0, 1, 0];
+  // cPat = [0, 0, 0, 0];
+  // bPat = [0, 0, 0, 0];
 
-  bass = loadSound('assets/bass_sample.mp3', () => {
-    
-  })
+  // hPhrase = new p5.Phrase('hh', (time) => {
+  //   hh.play(time)
 
+  // }, hPat)
+
+  // cPhrase = new p5.Phrase('clap', (time) => {
+  //   clap.play(time)
+
+  // }, cPat)
+
+  // bPhrase = new p5.Phrase('bass', (time) => {
+  //   bass.play(time)
+
+  // }, bPat)
+
+  // drums = new p5.Part()
+  
+  // drums.addPhrase(hPhrase)
+  // drums.addPhrase(cPhrase)
+  // drums.addPhrase(bPhrase)
+
+}
+
+function mouseClicked() {
   hPat = [1, 0, 1, 0];
-  cPat = [0, 0, 0, 0];
+  cPat = [0, 1, 0, 0];
   bPat = [0, 0, 0, 0];
 
   hPhrase = new p5.Phrase('hh', (time) => {
@@ -42,13 +67,19 @@ function setup() {
   drums.addPhrase(cPhrase)
   drums.addPhrase(bPhrase)
 
+  
 }
 
-function mouseClicked() {
-  if (drums.isPaying) {
-    drums.stop()
-  } else {
-    drums.loop()
+function keyPressed() {
+  if (key === ' ') {
+    if (hh.isLoaded() && clap.isLoaded() && bass.isLoaded()) {
+      if (!drums.isPlaying) {
+        drums.loop()
+      } else {
+        drums.stop()
+      }
+    } else {
+      console.log('Still loading')
+    }
   }
-  
 }
